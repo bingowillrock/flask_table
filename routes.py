f86c7eb39b9ed6b0521a3d041b7a8d4f06e1d5f4
bingowillrock <mynameisbingo@anglernook.com>
2024-05-12 23:23:15 +0100

    add button and csv file

--- routes.py
+++ routes.py
@@ -1,29 +1,25 @@
 from app import app
 from flask import render_template, request
 import csv
+import subprocess
 
 @app.route('/')
 def index():
-    # Read data from CSV file
-    data = []
-    with open('data.csv', newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            data.append(row)
-    
-    # Render template with data
-    return render_template('index.html', data=data)
+    return render_template('index.html')
 
+@app.route('/router_info', methods=['POST'])
+def router_info():
+    username = request.form['username']
+    with open('router_data.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        router_data = list(reader)
+    return render_template('router_info.html', username=username, router_data=router_data)
 
-
-
-# ''' username request'''
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# @app.route('/greet', methods=['POST'])
-# def greet():
-#     username = request.form['username']
-#     return render_template('greet.html', username=username)
+@app.route('/connect', methods=['POST'])
+def connect():
+    username = request.form['username']
+    ip_address = request.form['ip_address']
+    port = request.form['port']
+    command = f"ssh {username}@{ip_address} -p {port}"
+    subprocess.run(command, shell=True)
+    return "SSH connection started"
